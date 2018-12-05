b430342a03ea8d4029883ab11b6a4cbfd8b7838c
Praveen Udhayasuriyan <M1041646@mindtree.com>
2018-12-05 20:27:19 +0530

    more changes to cards

--- components/markets/marketCard.js
+++ components/markets/marketCard.js
@@ -9,10 +9,17 @@ const MarketsCard = props => {
             <p className="mb-0 font-weight-bold d-inline">{props.scrip}</p>
           </div>
           <div className="col-1 p-0 text-right m-auto">
-            <i
-              class="fa fa-chevron-down d-inline text-down"
-              aria-hidden="true"
-            />
+            <div className="text-down">
+              <i
+                class="fa fa-chevron-down directionArrow bounceAlpha1"
+                aria-hidden="true"
+              />
+              <i
+                class="fa fa-chevron-down directionArrow bounceAlpha2"
+                style={{ transform: "translateY(-10px)" }}
+                aria-hidden="true"
+              />
+            </div>
           </div>
           <div className="col-6 pl-0 text-center m-auto">
             <p className="mb-0 ml-1 d-inline font-weight-bold">{props.price}</p>
